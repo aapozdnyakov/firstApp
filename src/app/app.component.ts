cdf08cb10824d15938f9c800e315e7d10114fddf
Alexey Pozdnyakov <lesha.poz@yandex.ru>
2021-10-29 14:48:07 +0300

    Создали макет сайта на ангуляре

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,76 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstApp';
+  public participants = {
+    actors: [
+      {
+        img: "/assets/CarryCoon.png",
+        name: "Кэрри Кун",
+        enName: "Carrie Coon",
+        bio: "https://ru.wikipedia.org/wiki/Кун,_Кэрри",
+      },
+      {
+        img: "/assets/CarryCoon.png",
+        name: "Кэрри Кун",
+        enName: "Carrie Coon",
+        bio: "https://ru.wikipedia.org/wiki/Кун,_Кэрри",
+      },
+      {
+        img: "/assets/CarryCoon.png",
+        name: "Кэрри Кун",
+        enName: "Carrie Coon",
+        bio: "https://ru.wikipedia.org/wiki/Кун,_Кэрри",
+      },
+      {
+        img: "/assets/CarryCoon.png",
+        name: "Кэрри Кун",
+        enName: "Carrie Coon",
+        bio: "https://ru.wikipedia.org/wiki/Кун,_Кэрри",
+      },
+      {
+        img: "/assets/CarryCoon.png",
+        name: "Кэрри Кун",
+        enName: "Carrie Coon",
+        bio: "https://ru.wikipedia.org/wiki/Кун,_Кэрри",
+      },
+    ],
+    creators: [
+      {
+        img: "/assets/Bill Murray.png",
+        name: "Билл Мюррей",
+        enName: "Bill Murray",
+        bio: "https://ru.wikipedia.org/wiki/Мюррей,_Билл",
+      },
+      {
+        img: "/assets/Bill Murray.png",
+        name: "Билл Мюррей",
+        enName: "Bill Murray",
+        bio: "https://ru.wikipedia.org/wiki/Мюррей,_Билл",
+      },
+      {
+        img: "/assets/Bill Murray.png",
+        name: "Билл Мюррей",
+        enName: "Bill Murray",
+        bio: "https://ru.wikipedia.org/wiki/Мюррей,_Билл",
+      },
+      {
+        img: "/assets/Bill Murray.png",
+        name: "Билл Мюррей",
+        enName: "Bill Murray",
+        bio: "https://ru.wikipedia.org/wiki/Мюррей,_Билл",
+      },
+      {
+        img: "/assets/Bill Murray.png",
+        name: "Билл Мюррей",
+        enName: "Bill Murray",
+        bio: "https://ru.wikipedia.org/wiki/Мюррей,_Билл",
+      },
+    ],
+  };
+  public showCreators = false;
+  public toggleParticipants(creators: boolean) {
+    this.showCreators = creators
+
+  }
+
 }
